d60290b897e4f7776391d5234c9651fb66f7a3a1
Yuan Zhang <114724932+yuanzhang9@users.noreply.github.com>
2025-10-18 02:11:29 +0000

    Add random recipe endpoint to retrieve a random recipe

--- src/routes.js
+++ src/routes.js
@@ -40,4 +40,13 @@ router.post('/recipes/:id/edit', async (req, res) => {
 	res.redirect(`/recipes/${recipeId}`)
 })
 
+router.get('/recipes/random', async (req, res) => {
+	const db = await getDbConnection()
+	const recipe = await db.get('SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1')
+	res.render('recipe', { recipe })
+})
+	const randomIndex = Math.floor(Math.random() * recipes.length)
+	const recipe = recipes[randomIndex]
+	res.render('recipe', { recipe })
+})
 module.exports = router
